eadf45f7ee8ce5c7475f3c731c50b3d7945a36c3
Shifu Xu <steven.shifu.xu@gmail.com>
2016-03-10 10:27:27 -0500

    add random guess and svm alg

--- Midterm Report/RandomForest.py
+++ Midterm Report/RandomForest.py
@@ -1,58 +1,21 @@
-import numpy as np
+import ReadFiles
 import pandas as pd
+import utils
 from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
-from sklearn.metrics import mean_squared_error
-from math import sqrt
 
-def getRMSE(yTest, yPredict):
-    return sqrt(mean_squared_error(yTest, yPredict))
-
-#randomly selected about 80% of the training data from train.csv
-subTrainSet=pd.read_csv('subTrainSet.csv', encoding="ISO-8859-1")
-#randomly selected about 20% of the training data from train.csv
-developmentTestSet=pd.read_csv('developmentTestSet.csv', encoding="ISO-8859-1")
-
-#read the preprocessed features from file
-product_description_tfidf=pd.read_csv('product_description_tfidf.csv')
-product_tittle_tfidf=pd.read_csv('product_title_tfidf.csv')
-
-
-subTrainSetSize=subTrainSet.shape[0]
-
-
-AllSet = pd.concat((subTrainSet, developmentTestSet), axis=0, ignore_index=True)
-
-# add the features product_tittle_tfidf, product_description_tfidf
-AllSet = pd.merge(AllSet, product_tittle_tfidf, how='left', on='id')
-AllSet = pd.merge(AllSet, product_description_tfidf, how='left', on='id')
-
-#add another feature for prediction
-AllSet['search_words_nums'] = AllSet['search_term'].map(lambda x:len(x.split())).astype(np.int64)
-
-#drop columns not used for prediction
-AllSet = AllSet.drop(['product_title','search_term'], axis=1)
-
-#use the subtrain set as the training set
-trainSet = AllSet.iloc[:subTrainSetSize]
-trainSetFeatures = trainSet.drop(['id', 'relevance'], axis=1).values #id and relevance is not features to use
-trainSetLabels = trainSet['relevance'].values
+# read the data that we need from the original file
+trainSet, trainSetFeatures, trainSetLabels, testSet, testSetFeatures, testSetLabels = ReadFiles.readFiles()
 
 #train the model
 random_forest_regressor = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
 clf = BaggingRegressor(random_forest_regressor, n_estimators=45, max_samples=0.1, random_state=25)
 clf.fit(trainSetFeatures, trainSetLabels)
 
-#define the development test set as our test set
-testSet = AllSet.iloc[subTrainSetSize:]
-testSetFeatures = testSet.drop(['id', 'relevance'], axis=1).values
-testSetLabels = testSet['relevance'].values
-
 #make the prediction on the test set
 predictedLabels = clf.predict(testSetFeatures)
 
-
 #output the prediction
 id_test = testSet['id']
-pd.DataFrame({"id": id_test, "relevance": predictedLabels}).to_csv('prediction_for_developmentTestSet.csv', index=False)
+pd.DataFrame({"id": id_test, "relevance": predictedLabels}).to_csv('Random_Forest_Results.csv', index=False)
 
-print "RMSE :\t",getRMSE(testSetLabels,predictedLabels)
+print "RMSE :\t", utils.getRMSE(testSetLabels, predictedLabels)
--- Midterm Report/utils.py
+++ Midterm Report/utils.py
@@ -1,24 +1,11 @@
-import numpy as np
 from random import randint
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
-def getSamples(percent, matrix):
-    samples = np.shape(matrix)[0]
-    nums = int(round(samples * percent))
-    randIndexLst = []
-    target = []
-
-    while nums > 0:
-        temp = randint(0, samples - 1)
-        if temp not in randIndexLst:
-            randIndexLst.append(temp)
-            nums -= 1
-
-    for index in randIndexLst:
-        target.append(matrix[index])
-
-    return np.array(target)
-
+# cal the rmse between the actual label and predict label
 def getRMSE(yTest, yPredict):
     return sqrt(mean_squared_error(yTest, yPredict))
+
+# get the rand float between start and end
+def getRandFloat(start, end):
+    return float(randint(start * 100, end * 100 + 1)) / 100
